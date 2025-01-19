626f53ebe4c69033edf57e47b20d2555aa55104a
chitoge435 <dilla.umar.435@gmail.com>
2025-01-19 23:29:12 +0700

    api-contact

---
+++ app/Http/Controllers/Api/ContactController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $contacts = Contact::all();
+        return response()->json([
+            "data" => $contacts
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:contacts',
+                'no_telp' => 'required|string|max:255',
+                'massage' => 'required'
+            ]);
+            $contact = Contact::create([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'no_telp' => $validatedData['no_telp'],
+                'massage' => $validatedData['massage']
+            ]);
+            return response()->json([
+                "message" => "Data berhasil disimpan",
+                "data" => $contact
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                "message" => "Data gagal disimpan",
+                "errors" => $e->errors()
+            ], 400);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Contact $contact)
+    {
+        return response()->json([
+            "data" => $contact
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Contact $contact)
+    {
+        try {
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:contacts,email,' . $contact->id,
+                'no_telp' => 'required|string|max:255',
+                'massage' => 'required'
+            ]);
+            $contact->update([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'no_telp' => $validatedData['no_telp'],
+                'massage' => $validatedData['massage']
+            ]);
+            return response()->json([
+                "message" => "Data berhasil diubah",
+                "data" => $contact
+            ], 201);
+        } catch (ValidationException $e) {
+            return response()->json([
+                "message" => "Data gagal diubah",
+                "errors" => $e->errors()
+            ], 400);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Contact $contact)
+    {
+        $contact->delete();
+        return response()->json([
+            "message" => "Data berhasil dihapus"
+        ]);
+    }
+    public function showDelete()
+    {
+        $deletedContact = Contact::onlyTrashed()->get();
+        return response()->json([
+            "message" => "Data yang sudah dihapus",
+            "data" => $deletedContact
+        ], 201);
+    }
+    public function restore($id)
+    {
+        $contact = Contact::withTrashed()->where('id', $id)->first();
+        if ($contact) {
+            $contact->restore();
+            return response()->json([
+                "message" => "Data berhasil dikembalikan",
+                "data" => $contact
+            ], 201);
+        } else {
+            return response()->json([
+                "message" => "Data yang dipilih tidak ditemukan"
+            ], 404);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ContactController;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
@@ -12,3 +13,13 @@ Route::get('/user', function (Request $request) {
 Route::post('/register',[RegisterController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/contact', [ContactController::class, 'index']);
+    Route::post('/contact', [ContactController::class, 'store']);
+    Route::get('/contact/{contact}', [ContactController::class, 'edit']);
+    Route::put('/contact/{contact}', [ContactController::class, 'update']);
+    Route::delete('/contact/{contact}', [ContactController::class, 'destroy']);
+    Route::get('/contacts/deleted', [ContactController::class, 'showDelete']);
+    Route::put('/contact/{id}/restore', [ContactController::class, 'restore']);
+});
