bd88f3ecbed2fa20c9ebd410ce0c000ea9bc2ab8
chitoge435 <dilla.umar.435@gmail.com>
2025-01-19 21:13:07 +0700

    API-User

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,5 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Auth\RegisterController;
 Route::get('/', [HomeController::class,'index']);
