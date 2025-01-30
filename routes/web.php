821ab2ad69f4a6881ac08993549032ed23b87a36
chitoge435 <dilla.umar.435@gmail.com>
2025-01-30 20:44:03 +0700

    bariV2

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,21 @@
 
 use App\Http\Controllers\Auth\SPA\AuthController;
 use App\Http\Controllers\Auth\SPA\Dashboard\AdminController;
+use App\Http\Controllers\Dashboard\DashboardController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\UsersDataControllers;
 
-Route::get('/', function() {
-    return Inertia::render('Login');
-})->name('login');
 
-Route::post('/login',[AuthController::class,'login']);
-Route::post('/logout',[AuthController::class,'logout'])->middleware('auth');
+Route::middleware('guest')->group(function () {
+    Route::get('/', [HomeController::class,'index'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
 
-Route::middleware('auth', 'role:admin')->group( function () {
-    Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
+
+Route::middleware('auth', 'role:admin')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/userdata', [UsersDataControllers::class, 'index']);
 });
