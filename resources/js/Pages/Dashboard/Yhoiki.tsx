503df3657ff52c2d0859295cf0f627d08bb22ec9
chitoge435 <dilla.umar.435@gmail.com>
2025-01-31 05:39:17 +0700

    baruV3

---
+++ resources/js/Pages/Dashboard/Yhoiki.tsx
@@ -0,0 +1,12 @@
+import Logout from "@/Components/Dashboard/Logout";
+
+const dashboardYhoiki = () => {
+    return (
+        <div className="w-full h-screen flex flex-col justify-center items-center text-3xl">
+            <h1>Di Dashboard Yhoiki</h1>
+            <Logout />
+        </div>
+    );
+};
+
+export default dashboardYhoiki;
