4e037eece4c91ef761b148a4179a2e3afaac6bfc
chitoge435 <dilla.umar.435@gmail.com>
2025-01-31 07:55:30 +0700

    baruV5

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -8,21 +8,27 @@ use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function user() {
+    private $userData;
+
+    public function __construct()
+    {
         $user = Auth::user();
-        $roleName = $user->roles->first()->name;
-        return Inertia::render('Dashboard/User', [
+        $this->userData = [
             'name' => $user->name,
-            'role' => $roleName,
+            'role' => $user->roles->first()->name,
+        ];
+    }
+    public function user()
+    {
+        return Inertia::render('Dashboard/User', [
+            'data' => $this->userData,
         ]);
     }
 
-    public function Admin() {
-        $user = Auth::user();
-        $roleName = $user->roles->first()->name;
-        return Inertia::render('Dashboard/Admin', [
-            'name' => $user->name,
-            'role' => $roleName,
+    public function admin()
+    {
+        return Inertia::render('Dashboard/admin', [
+            'data' => $this->userData,
         ]);
     }
 }
